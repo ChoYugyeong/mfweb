0107d7ae3e8e74ccf75a0fba48b9872410c39d26
ChoYugyeong <89493845+ChoYugyeong@users.noreply.github.com>
2025-07-04 20:07:34 +0900

    all fix

---
+++ netlify/functions/config.js
@@ -0,0 +1,13 @@
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
+    })
+  };
+};
